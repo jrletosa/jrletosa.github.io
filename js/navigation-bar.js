f40758911536ef5816ea388497c46e9aaf9ce8e8
jrletosa <jrletosa@gmail.com>
2024-01-21 23:19:57 +0100

    * Change images

--- js/navigation-bar.js
+++ js/navigation-bar.js
@@ -16,14 +16,20 @@ function includeNavigationMenu(targetElementId) {
           <img src="../img/green-house.png" alt="Home" onclick="redirectToIndex()">
         </div>
         <li><a href="#" onclick="redirectToIndex()">${getTranslation("Home")}</a></li>
-        <li><a href="#" onclick="redirectToAbout()">${getTranslation("About")}</a></li>
+        <li><a href="#">${getTranslation("Wedding Day")}</a></li>
         <li class="submenu">
           <ul>
-            <li><a href="#">Subsection 1</a></li>
-            <li><a href="#">Subsection 2</a></li>
+            <li><a href="#" onclick="redirectToWeddingLocation()">${getTranslation("Location")}</a></li>
+            <li><a href="#" onclick="redirectToWeddingAgenda()">${getTranslation("Agenda")}</a></li>
+            <li><a href="#" onclick="redirectToWeddingDressCode()">${getTranslation("Dress Code")}</a></li>
           </ul>
         </li>
-        <li><a href="#" onclick="redirectToAbout()">${getTranslation("About 2")}</a></li>
+        <li><a href="#">${getTranslation("Brunch Day")}</a></li>
+        <li class="submenu">
+          <ul>
+            <li><a href="#" onclick="redirectToIndex()">${getTranslation("Brunch - Time & Location")}</a></li>
+          </ul>
+        </li>
       </div>
   `;
 
@@ -33,13 +39,30 @@ function includeNavigationMenu(targetElementId) {
 
   window.redirectToIndex = redirectToIndex;
   window.redirectToAbout = redirectToAbout;
+  window.redirectToWeddingLocation = redirectToWeddingLocation;
+  window.redirectToWeddingAgenda = redirectToWeddingAgenda;
+  window.redirectToWeddingDressCode = redirectToWeddingDressCode;
 }
 
-function redirectToAbout() {
-    window.location.href = 'about.html';
-}
 function redirectToIndex() {
   window.location.href = 'index.html';
+}
+
+function redirectToWeddingLocation() {
+  window.location.href = 'wedding-location.html';
+}
+
+function redirectToWeddingAgenda() {
+  window.location.href = 'wedding-agenda.html';
+}
+
+function redirectToWeddingDressCode() {
+  window.location.href = 'wedding-dress-code.html';
+}
+
+// jrletosa remove
+function redirectToAbout() {
+    window.location.href = 'about.html';
 }
 
 function includeCss(href) {
