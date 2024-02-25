6c72501283934b220ef04dfbf28a9e50d6315a2d
jrletosa <jrletosa@gmail.com>
2024-02-25 23:13:28 +0100

    revert changes

--- js/load-honey-moon-info.js
+++ js/load-honey-moon-info.js
@@ -11,17 +11,20 @@ function loadHoneyMoonInfo(targetElementId) {
   var div = document.createElement('div');
   div.classList.add("content");
   div.innerHTML = `
-    <h1>Honey Moon Upgrades</h1>
+    <h1>Honeymoon Upgrades</h1>
     
     <div class="space-between-h2-content"></div>
     
-    <p><b>Couple massage</b><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
+    <p>TBD</p>
     
-    <p><b>Flight Ticket upgrade</b><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
+    <!--<p><b>Couple massage</b><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
+    
+     <p><b>Flight Ticket upgrade</b><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
     
     <p><b>Hotel room upgrade</b><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
     
     <p><b>Dinner on the beach</b><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
+    -->
 `;
 
   targetElement.appendChild(div);
